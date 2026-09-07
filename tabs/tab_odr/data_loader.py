4ff167b032b318919a9b54d295ee015fcd38f3db
Vu Ngoc Lam <102470611+LamVu2001@users.noreply.github.com>
2026-09-07 11:48:45 +0700

    data_loader.py

--- tabs/tab_odr/data_loader.py
+++ tabs/tab_odr/data_loader.py
@@ -7,7 +7,6 @@ import gdown
 def get_odr_db(file_id: str = "1BCn1CH_VNWMslHxe1MQ4q9F2bJhbhY0o"):
     local_file = Path("TTS_phat_data.parquet")
 
-    # Tải file từ Google Drive nếu máy local chưa có
     if not local_file.exists():
         url = f"https://drive.google.com/uc?export=download&id={file_id}"
         with st.spinner("Đang tải dữ liệu TTS_phat_data.parquet..."):
@@ -15,14 +14,10 @@ def get_odr_db(file_id: str = "1BCn1CH_VNWMslHxe1MQ4q9F2bJhbhY0o"):
 
     con = duckdb.connect(database=':memory:')
     
-    # Đọc dữ liệu từ file Parquet và chuẩn hóa cột ngày
     con.execute(f"""
         CREATE VIEW orders AS 
         SELECT *, 
-               COALESCE(
-                   TRY_CAST(ngay_bat_dau_phai_phat AS DATE),
-                   TRY_CAST(ngay_trong_khoang AS DATE)
-               ) as clean_date
+               TRY_CAST(ngay_bat_dau_phai_phat AS DATE) as clean_date
         FROM read_parquet('{local_file}')
     """)
     return con
