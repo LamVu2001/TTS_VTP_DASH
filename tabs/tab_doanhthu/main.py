28f281f8788010d52c7794745b71a60f0a086bf5
Vu Ngoc Lam <102470611+LamVu2001@users.noreply.github.com>
2026-09-10 14:21:53 +0700

    main.py

--- tabs/tab_doanhthu/main.py
+++ tabs/tab_doanhthu/main.py
@@ -40,10 +40,9 @@ def render(file_id=None):
         conds = ["1=1"]
         
         # Sửa ép kiểu CAST AS DATE chuẩn xác giống Tab ODR
-        if exclude != "date" and isinstance(st.session_state.f_dt_date, (list, tuple)) and len(st.session_state.f_dt_date) == 2:
-            start_d = st.session_state.f_dt_date[0]
-            end_d = st.session_state.f_dt_date[1]
-            conds.append(f"CAST(clean_date AS DATE) BETWEEN '{start_d}' AND '{end_d}'")
+        if exclude != "date" and isinstance(st.session_state.f_date, (list, tuple)) and len(st.session_state.f_date) == 2:
+            conds.append(f"CAST(tg_ptc AS DATE) BETWEEN '{st.session_state.f_date[0]}' AND '{st.session_state.f_date[1]}'")
+
         
         if exclude != "kh":
             c = sql_in_clause("ma_khgui", st.session_state.f_dt_kh)
