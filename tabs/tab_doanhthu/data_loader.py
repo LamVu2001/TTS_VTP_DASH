c21209b7bf01cb9fd35ccc3ae96e4659dd070876
Vu Ngoc Lam <102470611+LamVu2001@users.noreply.github.com>
2026-09-10 11:52:35 +0700

    data_loader.py

--- tabs/tab_doanhthu/data_loader.py
+++ tabs/tab_doanhthu/data_loader.py
@@ -3,13 +3,6 @@ from tabs.tab_odr.data_loader import get_odr_db
 
 @st.cache_data
 def load_doanhthu_data():
-    # Khởi tạo kết nối DuckDB
     con = get_odr_db()
-    
-    # Query lấy dữ liệu từ VIEW 'orders' đã tạo ở tab_odr
-    query = """
-        SELECT *
-        FROM orders
-    """
-    df = con.execute(query).df()
-    return df
+    query = "SELECT * FROM orders"
+    return con.execute(query).df()
