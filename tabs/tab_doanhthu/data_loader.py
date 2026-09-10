5dc15f4d2bda6b7bcc08c9305bf550eb99b0b951
Vu Ngoc Lam <102470611+LamVu2001@users.noreply.github.com>
2026-09-10 11:56:49 +0700

    data_loader.py

--- tabs/tab_doanhthu/data_loader.py
+++ tabs/tab_doanhthu/data_loader.py
@@ -3,29 +3,23 @@ import duckdb
 import gdown
 import streamlit as st
 
-
 @st.cache_resource
-def get_doanhthu_db():
-  local_file = Path("TTS_phat_data.parquet")
+def get_doanhthu_db(file_id: str):
+    local_file = Path("TTS_doanhthu_data.parquet")
+    
+    if not local_file.exists():
+        url = f"https://drive.google.com/uc?export=download&id={file_id}"
+        gdown.download(url, str(local_file), quiet=False, use_cookies=False)
 
-  if not local_file.exists():
-    url = "https://drive.google.com/uc?export=download&id=1BCn1CH_VNWms1Hxe1MQ4q9F2bJhbhY0o"
-    gdown.download(url, str(local_file), quiet=False, use_cookies=False)
-
-  con = duckdb.connect(database=":memory:")
-  con.execute(f"""
+    con = duckdb.connect(database=":memory:")
+    con.execute(f"""
         CREATE VIEW orders AS 
         SELECT *,
             COALESCE(
                 TRY_CAST(ngay_bat_dau_phai_phat AS DATE),
                 TRY_CAST(STRPTIME(CAST(ngay_bat_dau_phai_phat AS VARCHAR), '%d/%m/%Y') AS DATE),
                 TRY_CAST(STRPTIME(CAST(ngay_bat_dau_phai_phat AS VARCHAR), '%Y-%m-%d') AS DATE)
-            ) as clean_date,
-            COALESCE(
-                TRY_CAST(ngay_phat_cuoi_cung AS DATE),
-                TRY_CAST(STRPTIME(CAST(ngay_phat_cuoi_cung AS VARCHAR), '%d/%m/%Y') AS DATE),
-                TRY_CAST(STRPTIME(CAST(ngay_phat_cuoi_cung AS VARCHAR), '%Y-%m-%d') AS DATE)
-            ) as clean_date_ptc
+            ) as clean_date
         FROM read_parquet('{local_file}')
     """)
-  return con
+    return con
