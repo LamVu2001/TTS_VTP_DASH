053a3c20bedc0535d8ca87ed7014a9ba9710dff2
Vu Ngoc Lam <102470611+LamVu2001@users.noreply.github.com>
2026-09-10 15:39:31 +0700

    data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -38,3 +38,30 @@ def get_connection(file_id: str = "1BCn1CH_VNWMslHxe1MQ4q9F2bJhbhY0o"):
     """)
     
     return con
+
+
+@st.cache_resource
+def get_opr_connection(file_id: str = "1OUeMwfOHuI1sOMU2ilQySYK2IOl8Xx4n"):
+    """
+    Hàm kết nối DuckDB dùng chung cho tất cả các Tab (ODR, Doanh Thu,...).
+    Tự động tải dữ liệu Parquet từ Google Drive nếu chưa có dưới local.
+    """
+    local_file = Path("TTS_thu_data.parquet")
+
+    # Tải file parquet nếu chưa tồn tại
+    if not local_file.exists():
+        url = f"https://drive.google.com/uc?export=download&id={file_id}"
+        with st.spinner("Đang tải dữ liệu TTS_phat_data.parquet..."):
+            gdown.download(url, str(local_file), quiet=False, use_cookies=False)
+
+    # Khởi tạo DuckDB In-Memory
+    con = duckdb.connect(database=':memory:')
+    
+    # Tạo VIEW orders chứa đầy đủ các trường dữ liệu và clean_date
+    con.execute(f"""
+        CREATE VIEW IF NOT EXISTS orders AS 
+        SELECT *
+        FROM read_parquet('{local_file}')
+    """)
+    
+    return con
