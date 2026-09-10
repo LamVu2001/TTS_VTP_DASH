ae78e4e6e6a57d02a4d36a3f9c83a5bfca57d228
Vu Ngoc Lam <102470611+LamVu2001@users.noreply.github.com>
2026-09-10 11:50:38 +0700

    data_loader.py

--- tabs/tab_doanhthu/data_loader.py
+++ tabs/tab_doanhthu/data_loader.py
@@ -1,2 +1,15 @@
-# Gọi dùng chung hàm load data/connection từ tab_odr
-from tabs.tab_odr.data_loader import load_data, get_connection
+import streamlit as st
+from tabs.tab_odr.data_loader import get_odr_db
+
+@st.cache_data
+def load_doanhthu_data():
+    # Khởi tạo kết nối DuckDB
+    con = get_odr_db()
+    
+    # Query lấy dữ liệu từ VIEW 'orders' đã tạo ở tab_odr
+    query = """
+        SELECT *
+        FROM orders
+    """
+    df = con.execute(query).df()
+    return df
